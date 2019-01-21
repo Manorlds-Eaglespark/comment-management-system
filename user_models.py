1009d43fa9c85ca1eda0c62903cfe417018adef7
sanya-kenneth <sanyakenneth@gmail.com>
2019-01-21 11:46:56 +0300

    Add user id to user model

--- user_models.py
+++ user_models.py
@@ -1,7 +1,10 @@
+users = {}
+
 class User:
     """ class for creating a user"""
     def __init__(self, first_name, last_name, user_name, user_password,
                  user_type, user_time_stamp):
+        self.user_id = len(users)+1
         self.first_name = first_name
         self.last_name = last_name
         self.user_name = user_name
@@ -13,6 +16,7 @@ class User:
     def to_json(self):
         """ Function returns user data in json format"""
         return {
+                "user_id": self.user_id,
                 "first_name": self.first_name,
                 "last_name": self.last_name,
                 "user_name": self.user_name,
