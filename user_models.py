d05a8015bf79412af8d3e324db21bd7fb410806c
sanya-kenneth <sanyakenneth@gmail.com>
2019-01-21 11:54:33 +0300

    Add make admin and make moderator methods

--- user_models.py
+++ user_models.py
@@ -3,15 +3,21 @@ users = {}
 class User:
     """ class for creating a user"""
     def __init__(self, first_name, last_name, user_name, user_password,
-                 user_type, user_time_stamp):
+                 user_time_stamp):
         self.user_id = len(users)+1
         self.first_name = first_name
         self.last_name = last_name
         self.user_name = user_name
         self.user_password = user_password
-        self.user_type = user_type
-        self.user_time_stamp = user_time_stamp
+        self.user_type = "normal"
 
+    def make_moderator(self):
+        """ method makes a normal user a moderator"""
+        self.user_type = "moderator"
+
+    def make_admin(self):
+        """ method makes a normal user an admin"""
+        self.user_type = "admin"
 
     def to_json(self):
         """ Function returns user data in json format"""
@@ -21,6 +27,5 @@ class User:
                 "last_name": self.last_name,
                 "user_name": self.user_name,
                 "user_password": self.user_password,
-                "user_type": self.user_type,
-                "user_time_stamp": self.user_time_stamp
+                "user_type": self.user_type
                 }
