8370a303db5e088e09a07eb4397a2be5254fc1f1
v1b3m <vibenjamin6@gmail.com>
2019-01-21 11:55:32 +0300

    Update user registration

--- user_models.py
+++ user_models.py
@@ -21,5 +21,4 @@ class User:
                 "user_name": self.user_name,
                 "user_password": self.user_password,
                 "user_type": self.user_type,
-                "user_time_stamp": self.user_time_stamp
                 }
